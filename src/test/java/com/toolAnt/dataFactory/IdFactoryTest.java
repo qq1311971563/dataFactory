daeaed851dd5d6959b7a87dc511628920883ef4d
马栋梁 <1311971563@qq.com>
2019-02-13 17:49:58 +0800

    细节完善

---
+++ src/test/java/com/toolAnt/dataFactory/IdFactoryTest.java
@@ -0,0 +1,30 @@
+package com.toolAnt.dataFactory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IdFactoryTest {
+
+    @Test
+    public void genId() {
+        System.out.println(IdFactory.genId("RI"));
+        System.out.println(IdFactory.genId("RI"));
+        System.out.println(IdFactory.genId("RI"));
+        System.out.println(IdFactory.genId("RI"));
+        System.out.println(IdFactory.genId("RI"));
+        System.out.println(IdFactory.genId("RI"));
+    }
+
+    @Test
+    public void genImageName() {
+    }
+
+    @Test
+    public void genItemId() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
